396cf3ffd1e3719bb7e8b162ebaec858bf53798a
ildanin <ilya.a.danilenko@gmail.com>
2026-04-18 18:34:56 +0300

    feat: add directories for networks

--- digit_drawer.py
+++ digit_drawer.py
@@ -38,7 +38,7 @@ test_x, test_y = load_data("mnist_test.csv")
 avg_x, avg_y = load_data("avg_digits.txt")
 
 #net = Network([10, 784], 'L_ReLU', 'sigmoid', factor_range=(-0.1, 0.1), bias_range=(-0.1, 0.1))
-net = load("drawer41.8.txt")
+net = load("drawers/41.8.txt")
 
 #Settings
 alpha = 0.1
@@ -50,36 +50,33 @@ net.train_stochastic_momentum(train_y, train_x, alpha, beta, 200, 60, True)
 
 avg_cost = test(net, test_y, test_x)
 print(f'Cost {round(avg_cost, 3)}')
-net.save(f'drawer{round(avg_cost, 2)}.txt')
+net.save(f"drawers/{round(100*avg_cost, 2)}.txt")
 
 inp = ''
 while inp != 'stop':
     inp = input()
-    if inp != 'stop' and inp != '':
-        if inp.split()[0] == 'train':
-            if len(inp.split()) == 4:
-                a, cycles, batch_size = inp.split()[1:]
-                net.train_stochastic(train_y, train_x, float(a), int(cycles), int(batch_size), True)
-            else:
-                print(f'Train takes 3 arguments, but {len(inp.split())-1} was given')
-        elif inp == 'test':
-            avg_cost = test(net, test_y, test_x)
-            print(f'Cost: {round(avg_cost, 3)}')
-        elif inp.split()[0] == 'save':
-            if len(inp) > 4:
-                net.save(inp.split()[1])
-                print(f'Saved {inp.split()[1]}')
-            else:
-                avg_cost = test(net, test_y, test_x)
-                net.save(f'drawer{round(avg_cost, 3)}.txt')
-                print(f'Saved drawer{round(avg_cost, 3)}')
-        elif all(char in 'q' for char in inp):
-            for _ in range(len(inp)):
-                garbage = random_array(0, 1, 10)
-                print(np.round(garbage, 2))
-                pyplot.imshow(get_digit(net, garbage, noise_range, threshold), cmap=pyplot.get_cmap('gray'))
-            pyplot.show(block = False)
-        elif inp.isdigit and len(inp) == 1:
-            pyplot.imshow(get_digit(net, np.array(data_transform(int(inp))), noise_range, threshold), cmap=pyplot.get_cmap('gray'))
-            print(f'Digit: {test_y[int(inp)].argmax()} | Cost to average: {round(net.cost(avg_x[int(inp)]), 3)}')
-            pyplot.show(block = False)
+    if inp == 'stop' or inp == '':
+        continue
+    if inp.split()[0] == 'train':
+        if len(inp.split()) == 4:
+            a, cycles, batch_size = inp.split()[1:]
+            net.train_stochastic(train_y, train_x, float(a), int(cycles), int(batch_size), True)
+        else:
+            print(f'Train takes 3 arguments, but {len(inp.split())-1} was given')
+    elif inp == 'test':
+        avg_cost = test(net, test_y, test_x)
+        print(f'Cost: {round(avg_cost, 3)}')
+    elif inp.split()[0] == 'save':
+        avg_cost = test(net, test_y, test_x)
+        net.save(f'drawers/{round(avg_cost, 3)}.txt')
+        print(f'Saved as {round(avg_cost, 3)} in drawers')
+    elif all(char in 'q' for char in inp):
+        for _ in range(len(inp)):
+            garbage = random_array(0, 1, 10)
+            print(np.round(garbage, 2))
+            pyplot.imshow(get_digit(net, garbage, noise_range, threshold), cmap=pyplot.get_cmap('gray'))
+        pyplot.show(block = False)
+    elif inp.isdigit and len(inp) == 1:
+        pyplot.imshow(get_digit(net, np.array(data_transform(int(inp))), noise_range, threshold), cmap=pyplot.get_cmap('gray'))
+        print(f'Digit: {test_y[int(inp)].argmax()} | Cost to average: {round(net.cost(avg_x[int(inp)]), 3)}')
+        pyplot.show(block = False)
--- digit_recognizer.py
+++ digit_recognizer.py
@@ -37,7 +37,7 @@ train_x, train_y = load_data("mnist_train.csv")
 test_x, test_y = load_data("mnist_test.csv")
 
 #net = Network([784, 64, 10], 'L_ReLU', factor_range=(-0.1, 0.1), bias_range=(-0.1, 0.1))
-net = load("recognizer96.71.txt")
+net = load("recognizers/96.71.txt")
 
 #Settings
 alpha = 0.04
@@ -47,42 +47,40 @@ net.train_stochastic_momentum(train_x, train_y, alpha, beta, 60000, 10, True)
 
 accuracy, avg_cost = test(net, test_x, test_y)
 print(f'Accuracy: {round(100 * accuracy, 2)}% | Cost {round(avg_cost, 3)}')
-net.save(f'recognizer{round(100 * accuracy, 2)}.txt')
+net.save(f'recognizers/{round(100 * accuracy, 2)}.txt')
 
 inp = ''
 while inp != 'stop':
     inp = input()
-    if inp != 'stop' and inp != '':
-        if inp.split()[0] == 'train':
-            if len(inp.split()) == 4:
-                a, cycles, batch_size = inp.split()[1:]
-                net.train_stochastic(train_x, train_y, float(a), int(cycles), int(batch_size), True)
-            else:
-                print(f'Train takes 3 arguments, but {len(inp.split())-1} was given')
-        elif inp == 'test':
-            accuracy, avg_cost = test(net, test_x, test_y)
-            print(f'Accuracy: {round(100 * accuracy, 2)}% | Cost: {round(avg_cost, 3)}')
-        elif inp.split()[0] == 'save':
-            if len(inp) > 4:
-                net.save(inp.split()[1])
-            else:
-                accuracy, avg_cost = test(net, test_x, test_y)
-                net.save(f'recognizer{round(100 * accuracy, 2)}.txt')
-        elif all(char in 'q' for char in inp):
-            for _ in range(len(inp)):
-                garbage = random_array(0, 1, (28, 28))
-                pyplot.imshow(garbage, cmap=pyplot.get_cmap('gray'))
-                print(net.process(garbage.flatten()), net.layer_results[-1].argmax())
-            pyplot.show(block = False)
-        elif all(char in 'r' for char in inp):
-            for _ in range(len(inp)):
-                rand = randint(0, 10000)
-                pyplot.imshow(test_x[rand].reshape(28, 28), cmap=pyplot.get_cmap('gray'))
-                net.process(test_x[rand])
-                print(f'{net.layer_results[-1].argmax() == test_y[rand].argmax()} | Neuro: {net.layer_results[-1].argmax()} | Answer: {test_y[rand].argmax()} | Cost: {round(net.cost(test_y[rand]), 3)} | ID: {rand}')
-            pyplot.show(block = False)
-        elif inp.isdigit():
-            pyplot.imshow(test_x[int(inp)].reshape(28, 28), cmap=pyplot.get_cmap('gray'))
-            net.process(test_x[int(inp)])
-            print(f'{net.layer_results[-1].argmax() == test_y[int(inp)].argmax()} | Neuro: {net.layer_results[-1].argmax()} | Answer: {test_y[int(inp)].argmax()} | Cost: {round(net.cost(test_y[int(inp)]), 3)} | Result: {net.layer_results[-1].round(3)}')
-            pyplot.show(block = False)
+    if inp == 'stop' and inp == '':
+        continue
+    if inp.split()[0] == 'train':
+        if len(inp.split()) == 4:
+            a, cycles, batch_size = inp.split()[1:]
+            net.train_stochastic(train_x, train_y, float(a), int(cycles), int(batch_size), True)
+        else:
+            print(f'Train takes 3 arguments, but {len(inp.split())-1} was given')
+    elif inp == 'test':
+        accuracy, avg_cost = test(net, test_x, test_y)
+        print(f'Accuracy: {round(100 * accuracy, 2)}% | Cost: {round(avg_cost, 3)}')
+    elif inp.split()[0] == 'save':
+        accuracy, avg_cost = test(net, test_x, test_y)
+        net.save(f'recognizers/{round(100 * accuracy, 2)}.txt')
+    elif all(char in 'q' for char in inp):
+        for _ in range(len(inp)):
+            garbage = random_array(0, 1, (28, 28))
+            pyplot.imshow(garbage, cmap=pyplot.get_cmap('gray'))
+            print(net.process(garbage.flatten()), net.layer_results[-1].argmax())
+        pyplot.show(block = False)
+    elif all(char in 'r' for char in inp):
+        for _ in range(len(inp)):
+            rand = randint(0, 10000)
+            pyplot.imshow(test_x[rand].reshape(28, 28), cmap=pyplot.get_cmap('gray'))
+            net.process(test_x[rand])
+            print(f'{net.layer_results[-1].argmax() == test_y[rand].argmax()} | Neuro: {net.layer_results[-1].argmax()} | Answer: {test_y[rand].argmax()} | Cost: {round(net.cost(test_y[rand]), 3)} | ID: {rand}')
+        pyplot.show(block = False)
+    elif inp.isdigit():
+        pyplot.imshow(test_x[int(inp)].reshape(28, 28), cmap=pyplot.get_cmap('gray'))
+        net.process(test_x[int(inp)])
+        print(f'{net.layer_results[-1].argmax() == test_y[int(inp)].argmax()} | Neuro: {net.layer_results[-1].argmax()} | Answer: {test_y[int(inp)].argmax()} | Cost: {round(net.cost(test_y[int(inp)]), 3)} | Result: {net.layer_results[-1].round(3)}')
+        pyplot.show(block = False)
